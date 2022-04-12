8362530045e8289d31f369798f3c5b686d77064d
GatteyG <GatteyGW18@student.cbhs.school.nz>
2022-04-12 22:17:34 +1200

    cardesign
    car

---
+++ CreateWithCodeP1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+     
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
